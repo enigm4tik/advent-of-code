9a320dc75546c149e1d6ed59382b91759fa02067
Klara Fall <anna.majewski@warad.org>
2021-12-03 16:37:27 +0100

    Add day 01 of advent of code 2020

---
+++ 2020/day01/day01.py
@@ -0,0 +1,38 @@
+# Advent of Code - 2020
+## Day 1 - Part 1
+
+with open('day01_input', 'r') as file:
+    lines = file.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+
+summand1 = 0
+summand2 = 0
+
+for x in lines: 
+    for y in lines:
+        if x == y: 
+            continue
+        else: 
+            if x + y == 2020: 
+                summand1 = x
+                summand2 = y
+                break
+
+print(f"Part 1 - Result: {summand1 * summand2}")
+
+## Day 1 - Part 2
+
+summand1 = 0
+summand2 = 0
+summand3 = 0
+
+for x in lines: 
+    for y in lines:
+        for z in lines:
+            if x != y != z and x + y + z == 2020: 
+                summand1 = x
+                summand2 = y
+                summand3 = z
+                break
+
+print(f"Part 2 - Result: {summand1 * summand2 * summand3}")
