c8aacbace967aadb84b7760a455972d7b6d8fb22
Klara Fall <anna.majewski@warad.org>
2021-12-12 01:46:15 +0100

    Fixed up some variables and prettified output

--- 2021/day11/day11.py
+++ 2021/day11/day11.py
@@ -1,7 +1,6 @@
 # Advent of Code - 2021
 ## Day 11 - Part 1
 
-from types import new_class
 import numpy as np 
 
 with open('puzzle_input', 'r') as file:
@@ -9,38 +8,29 @@ with open('puzzle_input', 'r') as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
 
-empty = np.empty([len(lines), len(lines[0])], int)
-shiny_array = np.full((len(lines), len(lines[0])), False, dtype=bool)
-can_shine_array = np.full((len(lines), len(lines[0])), True, dtype=bool)
+octopi = np.empty([len(lines), len(lines[0])], int)
+flashed = np.full((len(lines), len(lines[0])), False, dtype=bool)
 
 for index, line in enumerate(lines): 
-    for index2, letter in enumerate(line): 
-        empty[index][index2] = letter
+    for index2, character in enumerate(line): 
+        octopi[index][index2] = character
 
-def make_one_step(array, shiny_array, count, iteration):
+
+def iterate_one_step(array, shiny_array, count, iteration):
     if iteration == 0:
-        return array, count
+        return count
     increased_array = increase_energy(array)
     new_array, shiny_array = check_if_neighbors_shine(increased_array, shiny_array)
     count += len(shiny_array[shiny_array==True])
     fixed_array, fixed_shiny = fix_arrays(new_array, shiny_array)
-    return make_one_step(fixed_array, fixed_shiny, count, iteration-1)
+    return iterate_one_step(fixed_array, fixed_shiny, count, iteration-1)
 
-def part2(array, shiny_array, count, iteration):
-    print(iteration)
-    print(count)
-    if count == 100:
-        return array, iteration
-    increased_array = increase_energy(array)
-    new_array, shiny_array = check_if_neighbors_shine(increased_array, shiny_array)
-    count = len(shiny_array[shiny_array==True])
-    fixed_array, fixed_shiny = fix_arrays(new_array, shiny_array)
-    return part2(fixed_array, fixed_shiny, count, iteration+1)
 
 def fix_arrays(new_array, shiny_array):
-    new_array = np.where(new_array > 9, 0, new_array)
-    shiny_array = np.full((len(lines), len(lines[0])), False, dtype=bool)
+    new_array = np.where(new_array > 9, 0, new_array) # replace all 10s with 0 for next step
+    shiny_array = np.full((len(lines), len(lines[0])), False, dtype=bool) # reset all shining octopi to False
     return new_array, shiny_array
+
 
 def increase_energy(array):
     for index_i, value in enumerate(array):
@@ -48,24 +38,23 @@ def increase_energy(array):
             array[index_i,index_j] += 1
     return array
 
+
 def find_neighbor_coordinates(x, y):
-    # print(f"this is me: {(x, y)}")
     my_neighbors = []
     for i in range(-1, 2):
         for j in range(-1, 2):
             newx = x+j
             newy = y+i
-            # print(f"my new: {newx, newy}")
-            if newx < 0 or newy < 0:
+            if newx < 0 or newy < 0: # neighbor doesn't exist
                 continue
-            elif newx == x and newy == y: # this is me again
-                # print(f"this is me again!: {(newx, newy)}")
+            elif newx == x and newy == y: # this is me
                 continue
-            elif newx > 9 or newy > 9:
+            elif newx > len(lines)-1 or newy > len(lines[0])-1: # index out of range
                 continue
             else: 
                 my_neighbors.append((newx, newy))
     return my_neighbors
+
 
 def flash_my_neighbors(array, shiny_array, myx, myy):
     new_array = array
@@ -105,9 +94,21 @@ def check_if_neighbors_shine(array, shiny_array, shiny_neighbors=1):
                 new_array, shining_array = flash_my_neighbors(new_array, shining_array, x, y)
     return check_if_neighbors_shine(new_array, shining_array, shiny_neighbors)
 
-# print(make_one_step(empty, shiny_array, 0, 100))
-# make_one_step(empty, shiny_array, 0, 2)
+## Day 11 - Part 2
 
-print(part2(empty, shiny_array, 0, 0))
+def when_do_all_octopi_light_up(array, shiny_array, count, iteration):
+    if count == 100:
+        return iteration
+    increased_array = increase_energy(array)
+    new_array, shiny_array = check_if_neighbors_shine(increased_array, shiny_array)
+    count = len(shiny_array[shiny_array==True])
+    fixed_array, fixed_shiny = fix_arrays(new_array, shiny_array)
+    return when_do_all_octopi_light_up(fixed_array, fixed_shiny, count, iteration+1)
 
-# print(make_one_step(empty, shiny_array, 0, 36))
+part1 = iterate_one_step(octopi, flashed, 0, 100)
+
+print(f"Part 1 - Result: {part1}")
+
+part2 = when_do_all_octopi_light_up(octopi, flashed, 0, 1)
+
+print(f"Part 2 - Result: {part2}")
