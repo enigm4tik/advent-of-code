553e33b3de536fc6a9574c4ac9f1b730ffd4364b
Klara Fall <anna.majewski@warad.org>
2022-01-31 10:53:21 +0100

    Rename variables

--- 2015/day09/day09.py
+++ 2015/day09/day09.py
@@ -20,21 +20,21 @@ for line in input:
         distances[destination2][destination1] = int(distance)
 
 all_cities = distances.keys()
-a = permutations(all_cities)
+all_possible_paths = permutations(all_cities)
 cities = []
-for city in a: 
-    rev = city[::-1]
-    if not rev in cities: 
+for city in all_possible_paths: 
+    reverse_path = city[::-1]
+    if not reverse_path in cities: 
         cities.append(city)
 
 def calculate_distance(path):
-    bla = 0
+    distance = 0
     for index, city in enumerate(path): 
         if index == 0:
             continue
         else: 
-            bla += distances[path[index-1]][city]
-    return bla
+            distance += distances[path[index-1]][city]
+    return distance
 
 smallest_number = 100000000
 biggest_number = 0
