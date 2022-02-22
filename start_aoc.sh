a058ac4ac03c0291f829b986a3d0d3fdd4ac6451
Klara Fall <anna.majewski@warad.org>
2022-02-22 10:59:49 +0100

    Update shell script to start AOC year with sample code

--- start_aoc.sh
+++ start_aoc.sh
@@ -32,21 +32,34 @@ done
 
 year=$1;
 cd ~/Documents/advent-of-code/
+
 for i in {1..9}; 
 do
-    mkdir -p $year/day0$i;
-    touch $year/day0$i/day0$i.py 
-    echo "# Advent of Code - $year" >> $year/day0$i/day0$i.py 
-    echo "## Day $i" >> $year/day0$i/day0$i.py 
-    touch $year/day0$i/puzzle_input
+mkdir -p $year/day0$i;
+touch $year/day0$i/day0$i.py 
+echo "# Advent of Code - $year" >> $year/day0$i/day0$i.py 
+echo "## Day $i" >> $year/day0$i/day0$i.py 
+echo "
+with open('puzzle_input') as file:
+# with open('test_input') as file:
+    lines = file.readlines()" >> $year/day0$i/day0$i.py 
+touch $year/day0$i/puzzle_input
+touch $year/day0$i/test_input
 done
+
 for i in {10..25};
 do
-    mkdir -p $year/day$i;
-    touch $year/day$i/day$i.py 
-    echo "# Advent of Code - $year" >> $year/day$i/day$i.py 
-    echo "## Day $i" >> $year/day$i/day$i.py 
-    touch $year/day$i/puzzle_input
+mkdir -p $year/day$i;
+touch $year/day$i/day$i.py 
+echo "# Advent of Code - $year" >> $year/day$i/day$i.py 
+echo "## Day $i" >> $year/day$i/day$i.py 
+echo "
+with open('puzzle_input') as file:
+# with open('test_input') as file:
+    lines = file.readlines()" >> $year/day$i/day$i.py 
+touch $year/day$i/puzzle_input
+touch $year/day$i/test_input
 done
+
 echo "Created folder/file structure for Advent of Code $year"
 echo "Have fun!"
