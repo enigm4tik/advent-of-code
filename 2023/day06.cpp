7ec1173dff6e473baec75a1365cc2b5b1938e0fb
Anna <79857061+enigm4tik@users.noreply.github.com>
2023-12-07 00:13:35 +0100

    Replaced unsigned long long int with using BigInt

--- 2023/day06.cpp
+++ 2023/day06.cpp
@@ -1,26 +1,27 @@
 /** Advent of Code - 2023
 ** Day 6
 ** Implementation of a binary search in C++ as practice.
-** Improved my runtime over the python implementation by A LOT.
+** Improved my time over the python implementation by A LOT.
 ** I learned a lesson or two about big numbers...
 ** I could probably improve it a bit but it's late and I've proved my point :) 
 **/
 
 #include <iostream>
 using namespace std;
+using bigInt = unsigned long long int;
 
-unsigned long long int calculateDistance(int time, int speed){
-    unsigned long long int value = time - speed;
-    unsigned long long int travel_distance = speed * value;
+bigInt calculateDistance(bigInt time, bigInt speed){
+    bigInt value = time - speed;
+    bigInt travel_distance = speed * value;
     return travel_distance;
 }
 
-int findLeftMostCandidate(int &time, long long int &distance){
+int findLeftMostCandidate(int &time, bigInt &distance){
     int left = 0;
     int right = time/2;
     int current = (left + right)/2;
-    unsigned long long int calculatedDistance = calculateDistance(time, current);
-    unsigned long long int neighbor = calculateDistance(time, current - 1);
+    bigInt calculatedDistance = calculateDistance(time, current);
+    bigInt neighbor = calculateDistance(time, current - 1);
     while (!(calculatedDistance > distance && neighbor <= distance)){
         if (calculatedDistance < distance){
             left = current;
@@ -35,12 +36,12 @@ int findLeftMostCandidate(int &time, long long int &distance){
 }
 
 
-int findRightMostCandidate(int &time, long long int &distance){
+int findRightMostCandidate(int &time, bigInt &distance){
     int left = time/2;
     int right = time;
     int current = (left + right)/2;
-    unsigned long long int calculatedDistance = calculateDistance(time, current);
-    unsigned long long int neighbor = calculateDistance(time, current + 1);
+    bigInt calculatedDistance = calculateDistance(time, current);
+    bigInt neighbor = calculateDistance(time, current + 1);
     while (!(calculatedDistance > distance && neighbor <= distance)){
         if (calculatedDistance <= distance){
             right = current;
@@ -57,28 +58,28 @@ int findRightMostCandidate(int &time, long long int &distance){
 int part1() {
     const int length = 4;
     int time[length] = {35, 93, 73, 66};
-    long long int distance[length] = {212, 2060, 1201, 1044};
+    bigInt distance[length] = {212, 2060, 1201, 1044};
 
-    int resultContainer[length];
+    int part1[length];
     for (int i = 0; i < length; i++) {
         int result = 1;
         result -= findLeftMostCandidate(time[i], distance[i]);
         result += findRightMostCandidate(time[i], distance[i]);
-        resultContainer[i] = result;
+        part1[i] = result;
     }
 
     int product = 1;
-    for (int amount : resultContainer) {
+    for (int amount : part1) {
         product *= amount;
     }
     return product;
 }
 
-long long int part2(){
+bigInt part2(){
     const int length = 1;
     int time =  35937366;
-    long long int distance = 212206012011044;
-    long long int result = 1;
+    bigInt distance = 212206012011044;
+    bigInt result = 1;
     result -= findLeftMostCandidate(time, distance);
     result += findRightMostCandidate(time, distance);
 
@@ -89,4 +90,3 @@ int main() {
     cout << "Part 1: " << part1() << endl;
     cout << "Part 2: " << part2() << endl;
 }
-
