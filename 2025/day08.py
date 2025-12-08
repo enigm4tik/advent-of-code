370c1284d07d162b0f98b1f101445bd8a6093405
enigm4tik <anna.majewski@outlook.com>
2025-12-08 22:09:15 +0100

    Refactor day08 of Advent of Code 2025

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,4 +1,5 @@
-#day8
+# Advent of Code - 2025
+## Day 8
 
 from math import sqrt,pow
 
@@ -6,6 +7,7 @@ with open('input', 'r') as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
 
+# Globals to create names for my objects
 ITERATOR = 0
 NAMEITERATOR = 0
 
@@ -38,9 +40,6 @@ class Junction:
             if other != self:
                 calculated_distance = self.calculate_distance(other)
                 self.order[other,self] = calculated_distance
-    
-    def get_coordinates(self):
-        return (self.x, self.y, self.z)
 
 class Circuit:
     members=[]
@@ -56,25 +55,10 @@ class Circuit:
     def add_junction(self,junction):
         self.members.append(junction)
 
-    def calculate_distance_between_circuits(self,other):
-        for junction in self.members:
-            # print("Junction: ", junction)
-            for junction2 in other.members:
-                # print("Junction 2: ", junction2)
-                calculated_distance = junction.calculate_distance(junction2)
-                # print("Calculated Distance: ", calculated_distance)
-                if calculated_distance < self.smallest_distance:
-                    self.smallest_distance = calculated_distance
-                    self.closest_connection = other
-        # print("Smallest Distance: ", self.smallest_distance)
-        # print("=========")
-
     def merge_circuits(self, other):
-        print("Merging: ", self.name, " with ", other.name)
-        # print(self.members, " with ", other.members)
         for junction in other.members:
             if junction not in self.members: 
-                self.members.append(junction)
+                self.add_junction(junction)
         self.smallest_distance = 9999999999999
         
     def contains(self,junction):
@@ -84,11 +68,10 @@ class Circuit:
         return len(self.members)
     
     def __repr__(self):
-        return f"<Name: {self.name}, Members: {self.members}>"#, Best Distance: {self.smallest_distance}, Partner: {self.closest_connection.name}>"
+        return f"<Name: {self.name}, Members: {self.members}>"
     
     def __str__(self):
         return self.name
-
 
 all_circuits=[]
 all_junctions=[]
@@ -96,47 +79,7 @@ for line in lines:
     new_junction = Junction(line)
     new_circuit = Circuit(new_junction)
     all_junctions.append(new_junction)
-    aaa = all_junctions[:]
     all_circuits.append(new_circuit)
-
-def calculate_all_distances(list_of_circuits):
-    for circuit in list_of_circuits:
-        for circuit2 in list_of_circuits:
-            if circuit2 != circuit:
-                circuit.calculate_distance_between_circuits(circuit2)
-    sorted_circuits=sorted(list_of_circuits, key=lambda x: x.smallest_distance)
-    return sorted_circuits
-
-def update_circuits(list_of_circuits, one, other):
-    for circuit in list_of_circuits:
-        if circuit.closest_connection == other:
-            circuit.calculate_distance_between_circuits(one)
-        if circuit != one:
-            one.calculate_distance_between_circuits(circuit)
-    sorted_circuits=sorted(list_of_circuits, key=lambda x: x.smallest_distance)
-    # print(sorted_circuits)
-    return sorted_circuits
-
-# connections = 10 # 10 for example
-# all_circuits=calculate_all_distances(all_circuits)
-# print(all_circuits)
-# while connections > 1:
-#     circuit1 = all_circuits[0]
-#     circuit1.merge_circuits(circuit1.closest_connection)
-#     for circuit in all_circuits:
-#         if circuit.name==circuit1.closest_connection.name:
-#             all_circuits.remove(circuit)
-#     all_circuits = calculate_all_distances(all_circuits)
-#     connections-=1
-#     lengths=[circuit.get_size() for circuit in all_circuits]
-#     lengths = sorted(lengths)
-#     print(lengths)
- 
-# lengths=[circuit.get_size() for circuit in all_circuits]
-# lengths = sorted(lengths)
-# print(lengths)
-# part1 = lengths[-1]*lengths[-2]*lengths[-3]
-# print("Part 1: ", part1)
 
 all_orders={}
 for junction in all_junctions:
@@ -146,23 +89,17 @@ for junction in all_junctions:
     else: 
         all_orders.update(junction.order)
 
-# aaa = all_junctions[:]
-print(aaa)
 sorted_junctions = dict(sorted(all_orders.items(), key=lambda item: item[1]))
 
-# all_circuits = calculate_all_distances(all_circuits)
 connected = 0
 for index, element in enumerate(sorted_junctions.keys()):
     lengths=[circuit.get_size() for circuit in all_circuits]
     lengths = sorted(lengths)
     if connected==1000:
         part1 = lengths[-1]*lengths[-2]*lengths[-3]
-        print("Part 1: ", part1)
     if index %2==0:
         continue
     left, right = element
-    left_circuit=""
-    right_circuit=""
     for circuit in all_circuits: 
         if circuit.contains(left):
             left_circuit = circuit
@@ -171,11 +108,12 @@ for index, element in enumerate(sorted_junctions.keys()):
     if left_circuit==right_circuit:
         connected+=1
         continue
-    # print("Merging based off: ", left, right)
     left_circuit.merge_circuits(right_circuit)
     all_circuits.remove(right_circuit)
-    # print(all_circuits)
     if len([circuit.get_size() for circuit in all_circuits]) == 1:
-        print("Part 2: ", left.x*right.x)
+        part2=left.x*right.x
         break
     connected+=1
+
+print("Part 1: ", part1)
+print("Part 2: ", part2)
